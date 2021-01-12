b5c4c2bfbd59a3d34fb9ead06886d8ab3f477653
CharlieSwires <charlieswires@gmail.com>
2021-01-12 17:34:54 +0000

    Latest corrected indentation

--- src/main/java/com/charlie/currency/Currency.java
+++ src/main/java/com/charlie/currency/Currency.java
@@ -19,15 +19,15 @@ import javax.swing.SwingUtilities;
 
 
 public class Currency extends JPanel{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
 
-	private Currency() {
-		setBorder(BorderFactory.createLineBorder(Color.BLACK, 4));
+    private Currency() {
+        setBorder(BorderFactory.createLineBorder(Color.BLACK, 4));
 
-	}
+    }
 
     JLabel poundsLabel = new JLabel("Ammount in GBP:");
     JTextField pounds = new JTextField("100.00",10);
@@ -38,80 +38,80 @@ public class Currency extends JPanel{
     JLabel EURLabel = new JLabel("Ammount in EUR:");
     JTextField eur = new JTextField("",10);
 
-	JButton go = new JButton("Convert");
-	JFrame jfrm = new JFrame("Currency Converter");
-	static Currency pe;
+    JButton go = new JButton("Convert");
+    JFrame jfrm = new JFrame("Currency Converter");
+    static Currency pe;
 
 
 
 
 
-	class PaintDemo{
+    class PaintDemo{
 
-		PaintDemo(){
-		    Properties prop = new Properties();
-		    ClassLoader loader = Thread.currentThread().getContextClassLoader();           
-		    InputStream stream = loader.getResourceAsStream("app.properties");
-		    try {
+        PaintDemo(){
+            Properties prop = new Properties();
+            ClassLoader loader = Thread.currentThread().getContextClassLoader();           
+            InputStream stream = loader.getResourceAsStream("app.properties");
+            try {
                 prop.load(stream);
             } catch (IOException e1) {
                 throw new RuntimeException("Couldn't load app.properties file");
             }
-			jfrm.setSize(1000, 200);
-			jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            jfrm.setSize(1000, 200);
+            jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
-			jfrm.setLayout(new BorderLayout());
+            jfrm.setLayout(new BorderLayout());
 
-			JPanel temp = new JPanel();
-			temp.add(poundsLabel);		
-			temp.add(pounds);
-			temp.add(go);
-			go.addActionListener(new ActionListener() {
+            JPanel temp = new JPanel();
+            temp.add(poundsLabel);		
+            temp.add(pounds);
+            temp.add(go);
+            go.addActionListener(new ActionListener() {
 
-				@Override
-				public void actionPerformed(ActionEvent e) {
-				    try {
-                    aud.setText(""+Math.round(100*Double.parseDouble(pounds.getText()) *
-                            Double.parseDouble(prop.getProperty("GBPtoAUD","1.0").toString())/100.0));
-                    usd.setText(""+Math.round(100*Double.parseDouble(pounds.getText()) *
-                            Double.parseDouble(prop.getProperty("GBPtoUSD","1.0").toString()))/100.0);
-                    eur.setText(""+Math.round(100*Double.parseDouble(pounds.getText()) *
-                            Double.parseDouble(prop.getProperty("GBPtoEUR","1.0").toString()))/100.0);
-				    } catch (Exception e1) {
-				        throw new RuntimeException("One or more of the doubles used were invalid:"+
-				    "pounds="+pounds.getText()+
-				    " GBPtoAUD="+prop.getProperty("GBPtoAUD","1.0").toString()+
-				    " GBPtoUSD="+prop.getProperty("GBPtoUSD","1.0").toString()+
-				    " GBPtoEUR="+prop.getProperty("GBPtoEUR","1.0").toString());
-				    }
-				}
-			});
-			pe.add(AUDLabel);
-			pe.add(aud);
-			pe.add(USDLabel);
-			pe.add(usd);
-			pe.add(EURLabel);
-			pe.add(eur);
-			jfrm.add(temp, BorderLayout.NORTH);
-			jfrm.add(pe, BorderLayout.CENTER);
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    try {
+                        aud.setText(""+Math.round(100*Double.parseDouble(pounds.getText()) *
+                                Double.parseDouble(prop.getProperty("GBPtoAUD","1.0").toString())/100.0));
+                        usd.setText(""+Math.round(100*Double.parseDouble(pounds.getText()) *
+                                Double.parseDouble(prop.getProperty("GBPtoUSD","1.0").toString()))/100.0);
+                        eur.setText(""+Math.round(100*Double.parseDouble(pounds.getText()) *
+                                Double.parseDouble(prop.getProperty("GBPtoEUR","1.0").toString()))/100.0);
+                    } catch (Exception e1) {
+                        throw new RuntimeException("One or more of the doubles used were invalid:"+
+                                "pounds="+pounds.getText()+
+                                " GBPtoAUD="+prop.getProperty("GBPtoAUD","1.0").toString()+
+                                " GBPtoUSD="+prop.getProperty("GBPtoUSD","1.0").toString()+
+                                " GBPtoEUR="+prop.getProperty("GBPtoEUR","1.0").toString());
+                    }
+                }
+            });
+            pe.add(AUDLabel);
+            pe.add(aud);
+            pe.add(USDLabel);
+            pe.add(usd);
+            pe.add(EURLabel);
+            pe.add(eur);
+            jfrm.add(temp, BorderLayout.NORTH);
+            jfrm.add(pe, BorderLayout.CENTER);
 
-			jfrm.setVisible(true);
-		}
+            jfrm.setVisible(true);
+        }
 
-	}
-	public static void main(String args[]) throws IOException {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				if (args.length != 0) {
-					System.out.println("usage: java Currency");
-					System.exit(1);
-				}
-				pe = new Currency();
-				pe.new PaintDemo();
-			}
-		});
-	}
+    }
+    public static void main(String args[]) throws IOException {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                if (args.length != 0) {
+                    System.out.println("usage: java Currency");
+                    System.exit(1);
+                }
+                pe = new Currency();
+                pe.new PaintDemo();
+            }
+        });
+    }
 
 }
 
